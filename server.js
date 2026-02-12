0174af0806b8ae6a30a7e34b846c2135e7ee2158
matheus110506 <matheus110506@hotmail.com>
2026-02-12 19:25:33 -0300

    Add user routes and server response messages

--- server.js
+++ server.js
@@ -4,3 +4,26 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ mensagem: "API funcionando!" });
+});
+
+app.get('/usuarios', (req, res) => {
+    res.json([
+        { id: 1, nome: "Vinícius" },
+        { id: 2, nome: "Matheus" }
+    ]);
+});
+
+app.post('/usuarios', (req, res) => {
+    const novoUsuario = req.body;
+    res.status(201).json({
+        mensagem: "Usuário criado!",
+        usuario: novoUsuario
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
