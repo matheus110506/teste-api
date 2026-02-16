f8124b9e908c08bc405d93d05a8f804cf15aaf5d
matheus110506 <matheus110506@hotmail.com>
2026-02-15 22:01:27 -0300

    Add files via upload

---
+++ concluir_tarefa.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require "conexao.php";
+
+if (!isset($_SESSION["usuario_id"])) {
+    header("Location: login.php")
+    exit();
+}
+
+if (isset($_GET["id"])) {
+
+    $tarefa_id = (int) $_GET["id"];
+    $usuario_id = $_SESSION["usuario_id"];
+
+    $sql = "UPDATE tarefas
+            SET status = 'concluida'
+            WHERE id = ? AND usuario_id = ?";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ii", $tarefa_id, $usuario_id);
+
+    $stmt->execute();
+    $stmt->close();
+}
+
+$conn->close();
+
+header("Location: dashboard.php");
+exit();
+?>
